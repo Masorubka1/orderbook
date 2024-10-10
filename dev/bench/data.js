6c202f2e742dfba276e9355f2b0f002abe0dd0a6
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 20:32:57 +0000

    add Go Benchmark (go) benchmark result for 793e11890a7c2a31b9df6d9fd6f4eb9b17931f75

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728591398993,
+  "lastUpdate": 1728592377256,
   "repoUrl": "https://github.com/Masorubka1/orderbook",
   "entries": {
     "Go Benchmark": [
@@ -234,6 +234,242 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkOrderbook - allocs/op",
             "value": 28919816,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Masorubka1",
+            "username": "Masorubka1"
+          },
+          "committer": {
+            "name": "Masorubka1",
+            "username": "Masorubka1"
+          },
+          "id": "793e11890a7c2a31b9df6d9fd6f4eb9b17931f75",
+          "message": "Feature/implement bechmarks ci",
+          "timestamp": "2024-10-10T13:17:09Z",
+          "url": "https://github.com/Masorubka1/orderbook/pull/1/commits/793e11890a7c2a31b9df6d9fd6f4eb9b17931f75"
+        },
+        "date": 1728592377011,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLatency",
+            "value": 29999984880,
+            "unit": "ns/op\t       150.0 Add_Order_50_percentile_ms\t       330.0 Add_Order_75_percentile_ms\t       150.0 Add_Order_90_percentile_ms\t       331.0 Add_Order_95_percentile_ms\t       341.0 Add_Order_99.9_percentile_ms\t       160.0 Add_Order_99_percentile_ms\t        40.00 Cancel_Order_50_percentile_ms\t        60.00 Cancel_Order_75_percentile_ms\t        40.00 Cancel_Order_90_percentile_ms\t        60.00 Cancel_Order_95_percentile_ms\t        70.00 Cancel_Order_99.9_percentile_ms\t        40.00 Cancel_Order_99_percentile_ms\t4011627328 B/op\t16225789 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - ns/op",
+            "value": 29999984880,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_50_percentile_ms",
+            "value": 150,
+            "unit": "Add_Order_50_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_75_percentile_ms",
+            "value": 330,
+            "unit": "Add_Order_75_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_90_percentile_ms",
+            "value": 150,
+            "unit": "Add_Order_90_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_95_percentile_ms",
+            "value": 331,
+            "unit": "Add_Order_95_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_99.9_percentile_ms",
+            "value": 341,
+            "unit": "Add_Order_99.9_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Add_Order_99_percentile_ms",
+            "value": 160,
+            "unit": "Add_Order_99_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_50_percentile_ms",
+            "value": 40,
+            "unit": "Cancel_Order_50_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_75_percentile_ms",
+            "value": 60,
+            "unit": "Cancel_Order_75_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_90_percentile_ms",
+            "value": 40,
+            "unit": "Cancel_Order_90_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_95_percentile_ms",
+            "value": 60,
+            "unit": "Cancel_Order_95_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_99.9_percentile_ms",
+            "value": 70,
+            "unit": "Cancel_Order_99.9_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - Cancel_Order_99_percentile_ms",
+            "value": 40,
+            "unit": "Cancel_Order_99_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - B/op",
+            "value": 4011627328,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLatency - allocs/op",
+            "value": 16225789,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput",
+            "value": 30000000733,
+            "unit": "ns/op\t         0.01024 Throughput_50_percentile_ms\t         0.01027 Throughput_75_percentile_ms\t         0.01026 Throughput_90_percentile_ms\t         0.01028 Throughput_95_percentile_ms\t         0.01028 Throughput_99.9_percentile_ms\t         0.01028 Throughput_99_percentile_ms\t6860474528 B/op\t77138393 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - ns/op",
+            "value": 30000000733,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_50_percentile_ms",
+            "value": 0.01024,
+            "unit": "Throughput_50_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_75_percentile_ms",
+            "value": 0.01027,
+            "unit": "Throughput_75_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_90_percentile_ms",
+            "value": 0.01026,
+            "unit": "Throughput_90_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_95_percentile_ms",
+            "value": 0.01028,
+            "unit": "Throughput_95_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_99.9_percentile_ms",
+            "value": 0.01028,
+            "unit": "Throughput_99.9_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - Throughput_99_percentile_ms",
+            "value": 0.01028,
+            "unit": "Throughput_99_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - B/op",
+            "value": 6860474528,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - allocs/op",
+            "value": 77138393,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook",
+            "value": 29999980993,
+            "unit": "ns/op\t       331.0 Add_Order_Latencies_50_percentile_ms\t       140.0 Add_Order_Latencies_75_percentile_ms\t       380.0 Add_Order_Latencies_90_percentile_ms\t       171.0 Add_Order_Latencies_95_percentile_ms\t       200.0 Add_Order_Latencies_99.9_percentile_ms\t       190.0 Add_Order_Latencies_99_percentile_ms\t5078106096 B/op\t29314335 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - ns/op",
+            "value": 29999980993,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_50_percentile_ms",
+            "value": 331,
+            "unit": "Add_Order_Latencies_50_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_75_percentile_ms",
+            "value": 140,
+            "unit": "Add_Order_Latencies_75_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_90_percentile_ms",
+            "value": 380,
+            "unit": "Add_Order_Latencies_90_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_95_percentile_ms",
+            "value": 171,
+            "unit": "Add_Order_Latencies_95_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_99.9_percentile_ms",
+            "value": 200,
+            "unit": "Add_Order_Latencies_99.9_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - Add_Order_Latencies_99_percentile_ms",
+            "value": 190,
+            "unit": "Add_Order_Latencies_99_percentile_ms",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - B/op",
+            "value": 5078106096,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkOrderbook - allocs/op",
+            "value": 29314335,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
